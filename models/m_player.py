1955031818b8ac40fa85c6c30068bb699c0ce39a
Thomas Fricard <thomasfricard@gmail.com>
2021-01-04 15:03:47 +0100

    Reset du projet.

--- models/m_player.py
+++ models/m_player.py
@@ -1,12 +1,78 @@
+import re
+from typing import Union
+from datetime import date
+
+
 class ModelPlayer:
-    def __init__(self, last_name, first_name, birthdate, rank):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.birthdate = birthdate
-        self.rank = rank
+    def __init__(self, last_name: str, first_name: str, birthdate: Union[str, date], rank: int):
+        incorrect_values = set()
 
-        self.dict_player = {'last_name': self.last_name,
-                            'first_name': self.first_name,
-                            'birthdate': self.birthdate,
-                            'rank': self.rank}
+        try:
+            self.last_name = last_name
+        except ValueError:
+            incorrect_values.add("nom")
 
+        try:
+            self.first_name = first_name
+        except ValueError:
+            incorrect_values.add("prénom")
+
+        try:
+            self.birthdate = birthdate
+        except ValueError:
+            incorrect_values.add("date de naissance")
+
+        try:
+            self.rank = rank
+        except ValueError:
+            incorrect_values.add("classement")
+
+        if incorrect_values:
+            raise ValueError(f"Paramètres de model player incorrects: {incorrect_values}")
+
+    @property
+    def last_name(self) -> str:
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value: str):
+        if type(value) == str and re.match(r"^[A-Za-z '\-éèàîïùç]{2,}$", value):
+            self.__last_name = value
+        else:
+            raise ValueError()
+
+    @property
+    def first_name(self) -> str:
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, value: str):
+        if type(value) == str and re.match(r"^[A-Za-z '\-éèàîïùç]{2,}$", value):
+            self.__first_name = value
+        else:
+            raise ValueError()
+
+    @property
+    def birthdate(self) -> str:
+        return self.__birthdate
+
+    @birthdate.setter
+    def birthdate(self, value: Union[str, date]):
+        if type(value) == str and re.match(r"^(\d{4})-(\d{2})-(\d{2})$", value):
+            date.fromisoformat(value)
+            self.__birthdate = value
+        elif type(value) == date:
+            self.__birthdate = value
+        else:
+            raise ValueError()
+
+    @property
+    def rank(self) -> int:
+        return self.__rank
+
+    @rank.setter
+    def rank(self, value: int):
+        if type(value) == int:
+            self.__rank = value
+        else:
+            raise ValueError()
--- utility.py
+++ utility.py
@@ -1,16 +1,84 @@
 import re
+from datetime import date, datetime
 
 
-def name_regex(name_input):
-    name = re.match(r"^[A-Z][A-Za-z '-]+$", name_input)
-    return name
+def ask_pattern(user_input: str) -> str:
+    while True:
+        try:
+            u_input = input(user_input)
+        except ValueError:
+            print("Format non valide.")
+        else:
+            if re.match(r"^[A-Za-z '\-éèàîïùç]{2,}$", u_input):
+                return u_input
+            else:
+                print("Format non valide. 2 Caractères minimum. Les chiffres ne sont pas autorisés")
 
 
-def date_regex(date_input):
-    date = re.match(r"^(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})$", date_input)
-    return date
+def ask_date(user_input: str) -> date:
+    while True:
+        try:
+            d_input = input(user_input)
+        except ValueError:
+            print("Format de date non valide.")
+        else:
+            if re.match(r"^(\d{4})-(\d{2})-(\d{2})$", d_input):
+                try:
+                    r_date = date.fromisoformat(d_input)
+                    return r_date
+                except ValueError as error:
+                    print("Date non valide:", error)
+            elif re.match(r"^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})$", d_input):
+                try:
+                    r_datetime = datetime.fromisoformat(d_input)
+                    return r_datetime
+                except ValueError as error:
+                    print("Date non valide:", error)
+            else:
+                print("Format de date non valide.")
 
 
-def birthdate_regex(birthdate_input):
-    birthdate = re.match(r"^(\d{2})-(\d{2})-(\d{4})$", birthdate_input)
-    return birthdate
+def ask_choices(choices: list) -> int:
+    while True:
+        print("Entrez le chiffre de votre choix: ")
+        for i, choice in enumerate(choices):
+            print("{0} - {1}".format(i+1, choice))
+        try:
+            user_input = int(input())
+        except ValueError:
+            print("Veuillez entrer un chiffre.")
+        else:
+            if 1 <= user_input <= len(choices):
+                return user_input
+            else:
+                print("Cette option n'existe pas.")
+
+
+def ask_int(user_input: str) -> int:
+    while True:
+        try:
+            r_input = int(input(user_input))
+        except ValueError:
+            print("Seuls des chiffres sont autorisés.")
+        else:
+            if r_input < 0:
+                print("Veuillez entrer une valeur positive.")
+                continue
+            else:
+                return r_input
+
+
+def qcm(options: list) -> int:
+    while True:
+        print("Enter the number of your choice - ")
+        for i, option in enumerate(options):
+            print(f"{i+1} - {option}")
+        try:
+            answer = int(input())
+        except ValueError:
+            print("Not an number !")
+        else:
+            if 1 <= answer <= len(options):
+                return answer
+            else:
+                print("This option doesn't exist !")
