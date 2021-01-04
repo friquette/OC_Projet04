990c7aa647fb2b770419338404187abe47ef1a89
Thomas Fricard <thomasfricard@gmail.com>
2021-01-04 15:12:23 +0100

    Test du module player et des fonctions utilitaires

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import vues.v_menu as menu
+import models.m_player as m_player
+import utility as utl
 
-menu.display_menu()
+last_name = utl.ask_pattern("Nom: ")
+first_name = utl.ask_pattern("Prenom: ")
+birthdate = utl.ask_date("Date de naissance: ")
+rank = utl.ask_int("Classement: ")
+
+player = m_player.ModelPlayer(last_name, first_name, birthdate, rank)
+
+print("Joueur: {} {}, né le {}. Classement: {}".format(player.last_name,
+                                                       player.first_name,
+                                                       player.birthdate,
+                                                       player.rank))
