73b92dfebfdefb4b246c88d3fc9f33bc57cc7128
Thomas Fricard <thomasfricard@gmail.com>
2021-01-07 14:58:44 +0100

    simplification du controller player et adaptation du view player

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import views.v_menu as v_menu
 import views.v_player as v_player
 import views.v_menu as v_menu
 
-"""player = v_player.PlayerView()
-player.display_player_questions()"""
+player = v_player.PlayerView()
+player.display_player_questions()
 
-menu = v_menu.MenuView()
-menu.get_user_choice()
+"""menu = v_menu.MenuView()
+menu.get_user_choice()"""
--- views/v_player.py
+++ views/v_player.py
@@ -1,16 +1,13 @@
-import utility as utils
 import controllers.c_player as c_player
 
 
 class PlayerView:
     def __init__(self):
-        # self.utils = utils.Utils()
         self.c_player = c_player.PlayerController()
-        self.last_name = None
-        self.first_name = None
-        self.birthdate = None
-        self.rank = None
 
     def display_player_questions(self):
-        self.c_player.start()
-        print("test: ", self.c_player)
+        self.c_player.get_player_info()
+        print("Joueur: {} {}, né le {}. Classement: {}".format(self.c_player.first_name,
+                                                               self.c_player.last_name,
+                                                               self.c_player.birthdate,
+                                                               self.c_player.rank))
