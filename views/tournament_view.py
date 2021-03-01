2d9e70fc0027ccf0b83263ae46df6a8cf75b3279
Thomas Fricard <thomasfricard@gmail.com>
2021-03-01 10:17:26 +0100

    supression d'un print inutile

--- views/tournament_view.py
+++ views/tournament_view.py
@@ -38,14 +38,13 @@ class TournamentView:
         name = self.utils.ask_pattern('Nom: ')
         location = self.utils.ask_pattern('Lieu: ')
         date = self.utils.ask_date('Date (AAAA-MM-JJ HH:MM): ', False)
-        print(type(date))
         identifier = f"{location}{date}"[:-9]
         print("Contrôle du temps: ")
         time_rule = self.utils.ask_choices(list(TimeRule))
         description = self.utils.ask_pattern('Description: ')
         nb_round = self.utils.ask_int('Nombre de round: ')
 
-        for i in range(8):
+        """for i in range(8):
             while True:
                 user_id_choice = self.utils.ask_identifier(f"Id du joueur {i + 1}: ")
                 id_user = player_manager.find_player_by_id(user_id_choice)
@@ -64,7 +63,17 @@ class TournamentView:
                     break
                 else:
                     print("Ce joueur est déjà présent dans ce tournoi. Veuillez entrer un nouveau joueur.")
-                    continue
+                    continue"""
+
+        preset_players = ["b03017d2-a3a1-406c-af08-88d0be93e356", "11dc9bec-3d10-4312-982f-754a26b475a2",
+                          "1c1590f2-1484-4d02-b194-c17a5ffdeb28", "b3df014b-3975-4313-a196-d9760d8b8353",
+                          "de5352cf-3fff-4236-a2fc-3b38a2d07748", "d451280e-a734-4936-939f-64034d22d828",
+                          "4ed6aa39-d91b-4209-bbb9-e1a83a24140d", "ad39f897-7fad-4217-aefd-bf8243c7c759"]
+
+        for p_player in preset_players:
+            id_user = player_manager.find_player_by_id(p_player)
+            players.append(id_user)
+            new_player_list.append([id_user, .0])
 
         print("~~Début du tournoi~~")
         self.generate_rounds(new_player_list, nb_round)
